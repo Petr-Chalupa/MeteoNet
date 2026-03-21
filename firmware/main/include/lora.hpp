c08beb42d7cfab15659502fc9deb3c77a1f0e920
Petr Chalupa <chalupapetr77@gmail.com>
2026-03-21 21:36:02 +0100

    Comm headers

---
+++ firmware/main/include/lora.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace LoRa {
+
+struct Packet {
+  const uint8_t *data;
+  size_t size;
+};
+
+using RX_CB = void (*)(Packet);
+
+/**
+ * Initializes the LoRa module.
+ */
+void init();
+
+/**
+ * Deinitializes the LoRa module.
+ */
+void deinit();
+
+/**
+ * Checks if the LoRa module is ready.
+ * @return true  If the LoRa module is initialized and ready
+ * @return false Otherwise
+ */
+[[nodiscard]] bool is_ready();
+
+/**
+ * Sends packet via LoRa
+ * @param data  The data to send
+ * @param flush Whether to send whole TX data buffer immediately
+ * @return true  If the packet was successfully put into TX buffer
+ * @return false Otherwise
+ */
+[[nodiscard]] bool tx(const Packet &data, bool flush);
+
+/**
+ * Registers callback for data RX
+ * @param cb What function to use
+ */
+void rx(RX_CB cb);
+
+} // namespace LoRa
--- firmware/main/include/wifi.hpp
+++ firmware/main/include/wifi.hpp
@@ -1,15 +1,49 @@
 #pragma once
 
-/**
- * Initializes the WiFi stack and attempts to connect
- * @param ssid The string containing the WiFi Network Name.
- * @param pwd  The string containing the WiFi Password.
- */
-void wifi_init(char *ssid, char *pwd);
+#include <stdint.h>
+
+namespace WiFi {
+
+struct Packet {
+  const uint8_t *data;
+  size_t size;
+};
+
+using RX_CB = void (*)(Packet);
 
 /**
- * Checks the current network connectivity status
- * @return true  If the station is connected
- * @return false Othrewise
+ * Initializes the WiFi stack and attempts to connect.
+ * Connection attempt is retried 5 times.
+ * @param ssid The string containing the WiFi network name
+ * @param pwd  The string containing the WiFi password
  */
-bool wifi_is_connected();
+void init(const char *ssid, const char *pwd);
+
+/**
+ * Disconnects from WiFi network and deinitializes the WiFi stack
+ */
+void disconnect();
+
+/**
+ * Checks the current network connectivity status.
+ * @return true  If the station is connected
+ * @return false Otherwise
+ */
+[[nodiscard]] bool is_connected();
+
+/**
+ * Sends packet via WiFi
+ * @param data  The data to send
+ * @param flush Whether to send whole TX data buffer immediately
+ * @return true  If the packet was successfully put into TX buffer
+ * @return false Otherwise
+ */
+[[nodiscard]] bool tx(const Packet &data, bool flush);
+
+/**
+ * Registers callback for data RX
+ * @param cb What function to use
+ */
+void rx(RX_CB cb);
+
+} // namespace WiFi
